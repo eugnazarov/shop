df464d0bee18601d53701c6c023667c52f759b69
eugene@git config --global user.name eugenegit config --global user.name eugene@ <eugnazaroff@gmail.com>
2021-05-01 18:36:33 +0400

    fixed index,added registration

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,6 @@
-/*let button = document.getElementById('burger');
-
-let navigation = document.getElementById('menu');
-
-let wrp = document.getElementById('wrp');
-
-button.addEventListener('click', function(){
-    navigation.classList.toggle('display-block');
-    wrp.classList.toggle('display-block');
-});*/
-
 let headerButton = document.querySelector(".header__menu-button");
 let modal = document.querySelector('.modal');
 let closeButton = document.querySelector('.modal__menu-exit-btn')
-let black = document.querySelector('.modal__black')
 closeButton.addEventListener('click', () =>{
     modal.classList.toggle('modal--open')
 })
